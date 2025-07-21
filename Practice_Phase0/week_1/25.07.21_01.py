1e6afdd5c1da497e3fbd774a62fb21295b4b9ba0
kimjisoo0319 <kimjisoo5819@gmail.com>
2025-07-21 21:01:49 +0900

    Day1 _ 파이썬 설치 및 변수/출력 학습 완료

---
+++ Practice_Phase0/week_1/25.07.21_01.py
@@ -0,0 +1,15 @@
+character_name = "Geroge"
+character_age = "70"
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old.")
+print("He really liked the same john")
+print("but didn't like being 70.")
+
+character_name = "Geroge"
+character_age = "70"
+
+print(f"There once was a man named {character_name}, ")
+print(f"he was {character_age} years old.")
+print(f"He really liked the same {character_name}")
+print(f"but didn't like being {character_age}.")
