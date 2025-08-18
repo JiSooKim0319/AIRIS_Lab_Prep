f94d6139bda7e682b87337f5f0fe3e3887ebb536
kimjisoo0319 <kimjisoo5819@gmail.com>
2025-08-18 18:01:09 +0900

    Welcome

--- Practice_Phase0/week_2/25.08.18_08/8-6_Quiz.py
+++ Practice_Phase0/week_2/25.08.18_08/8-6_Quiz.py
@@ -11,20 +11,28 @@ from fileinput import filename
 
 # 조건 : 파일명은 "1주차.txt, "2주차.txt", ...와 같이 만듭니다.
 
-for num in range(1, 51):
-    print(" - " + str(num) + "주차 주간 보고 - ")
-    print("부서 :")
-    print("이름 :")
-    print("업무 요약 : ")
-    print()
+# for num in range(1, 51):
+#     print(" - " + str(num) + "주차 주간 보고 - ")
+#     print("부서 :")
+#     print("이름 :")
+#     print("업무 요약 : ")
+#     print()
+#
+#     filename = f"{num}주차.txt"
+#
+#     content = (
+#         f"- {num} 주차 주간 보고 -\n"
+#         "부서 : \n"
+#         "이름 : \n"
+#         "업무 요약 :\n"
+#     )
+#     with open(filename,"w", encoding="utf-8" ) as file:
+#         file.write(content)
 
-    filename = f"{num}주차.txt"
 
-    content = (
-        f"- {num} 주차 주간 보고 -\n"
-        "부서 : \n"
-        "이름 : \n"
-        "업무 요약 :\n"
-    )
-    with open(filename,"w", encoding="utf-8" ) as file:
-        file.write(content)
+for i in range(1, 51):
+    with open(str(i) + "주차.txt", "w", encoding="utf-8") as report_file:
+        report_file.write("- {0} 주차 주간보고 -".format(i))
+        report_file.write("\n 부서:")
+        report_file.write("\n 이름:")
+        report_file.write("\n 업무 요약:")
