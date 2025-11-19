aff0373e829d0356da31e7eade47aa210add0f02
kimjisoo0319 <kimjisoo5819@gmail.com>
2025-11-19 15:56:18 +0900

    Welcome

--- Suno_Enhance_Project/00_Reserch_lab/20251118_A_IO_sf.read.py
+++ Suno_Enhance_Project/00_Reserch_lab/20251118_A_IO_sf.read.py
@@ -18,3 +18,41 @@ try:
 
 except Exception as e:
     print(f"Error: {e}")
+ #--------------------------------------------------------------------------------
+# 3) `if`문과 함께 사용하기 (파일 검사)
+# sf.read()는 pathlib의 .exists() 및 if 문과 함께 사용하여 코드를 안전하게 만듬.
+
+# 사용자의 목표였던 ".wav"파일만 골라서 읽는 예제.
+p = Path("song_01.wav")
+
+    # 1. 존재하는지, 그리고 파일인지 확인
+if p.exists() and p.is_file():
+    # 2. 확장자(suffix)가 .wav인지 확인
+    if p.suffix == "wav.":
+        print(f"'{p.name}'은 WAV파일입니다. 읽기를 시도합니다.")
+        try:
+            data, samplerate = sf.read(p)
+            print(f"읽기 성공! 샘플레이트 : {samplerate} Hz")
+
+            # 3. 샘플레이트 값으로 추가 검사
+            if samplerate != 44100:
+                print(f"[경고] 이 파일은 표준 CD음질 (44100Hz)가 아닙니다.")
+        except Exception as e:
+            print(f"{p.name}' 파일 읽기 실패:{e}")
+    elif p.suffix == "mp3":
+        print(f"'{p.name}'은 MP3 파일입니다. 읽기를 시도합니다.")
+        try:
+            data, samplerate = sf.read(p)
+            print(f"읽기 성공! 샘플레이트 : {samplerate} Hz")
+
+            # 3. 샘플레이트 값으로 추가 검사
+            if samplerate != 44100:
+                print(f"[경고] 이 파일은 표준 CD음질 (44100Hz)가 아닙니다.")
+        except Exception as e:
+            print(f"{p.name}' 파일 읽기 실패:{e}")
+    else:
+        print(f"'{p.name}'는 WAV or MP3 파일이 아닙니다. (확장자: {p.suffix})")
+else:
+    print(f"'{p.name}' 파일을 찾을 수 없습니다.")
+
+
